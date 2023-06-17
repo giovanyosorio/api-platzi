6b58e16529c0b1f9c2b49092ba68727f0eedeade
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-17 18:21:35 -0500

    callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,16 @@ function calc(num1,num2,callback){
 }
 
 console.log(calc(3,5,sum));
+
+
+
+setTimeout(() => {
+    console.log("hola mundo");
+}, 2000);
+
+
+function saludo(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(saludo,3000,"gio")
